07459ad45c6c41d2cbf95d6ce528e66b49522144
Mahesa-07 <123784639+Mahesa-07@users.noreply.github.com>
2026-05-25 22:10:20 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,41 @@
 
+self.addEventListener(
+  "install",
+  event=>{
+
+    event.waitUntil(
+
+      caches.open("mathnawi-v1")
+      .then(cache=>{
+
+        return cache.addAll([
+          "./",
+          "./index.html",
+          "./style.css",
+          "./app.js"
+        ]);
+
+      })
+
+    );
+
+  }
+);
+
+self.addEventListener(
+  "fetch",
+  event=>{
+
+    event.respondWith(
+
+      caches.match(event.request)
+      .then(response=>{
+
+        return response || fetch(event.request);
+
+      })
+
+    );
+
+  }
+);
