f19d328821015f5eaeddc729ec31879f7db8e07f
Mahesa-07 <123784639+Mahesa-07@users.noreply.github.com>
2026-05-28 17:10:54 +0700

    Add files via upload

--- js/data/bab2.js
+++ js/data/bab2.js
@@ -8,83 +8,103 @@ export const bab2 = {
 [
   {
     "no": 35,
-    "text": "Wahai sahabat-sahabatku, dengarkanlah kisah ini: sesungguhnya ia adalah sumsum dari keadaan batin kita.³⁵"
+    "text": "Wahai sahabat-sahabatku, dengarkanlah kisah ini: sesungguhnya ia adalah sumsum dari keadaan batin kita.³⁵",
+    "audio":"../audio/bab2/bait35.mp3"
   },
   {
     "no": 36,
-    "text": "Dahulu kala ada seorang raja yang memiliki kekuatan duniawi dan kekuatan ruhani."
+    "text": "Dahulu kala ada seorang raja yang memiliki kekuatan duniawi dan kekuatan ruhani.",
+     "audio":"../audio/bab2/bait36.mp3"
   },
   {
     "no": 37,
-    "text": "Terjadilah bahwa pada suatu hari ia berkendara bersama para pengiringnya untuk berburu."
+    "text": "Terjadilah bahwa pada suatu hari ia berkendara bersama para pengiringnya untuk berburu.",
+     "audio":"../audio/bab2/bait37.mp3"
   },
   {
     "no": 38,
-    "text": "Di jalan raya, raja melihat seorang budak perempuan: jiwa raja terpesona olehnya."
+    "text": "Di jalan raya, raja melihat seorang budak perempuan: jiwa raja terpesona olehnya.",
+     "audio":"../audio/bab2/bait38.mp3"
   },
   {
     "no": 39,
-    "text": "Karena burung itu, jiwanya, beterbangan di dalam sangkar, dia memberi uang dan membeli budak perempuan itu."
+    "text": "Karena burung itu, jiwanya, beterbangan di dalam sangkar, dia memberi uang dan membeli budak perempuan itu.",
+     "audio":"../audio/bab2/bait39.mp3"
   },
   {
     "no": 40,
-    "text": "Setelah ia membelinya dan mencapai keinginannya, oleh takdir Ilahi perempuan itu pun jatuh sakit.⁴⁰"
+    "text": "Setelah ia membelinya dan mencapai keinginannya, oleh takdir Ilahi perempuan itu pun jatuh sakit.⁴⁰",
+     "audio":"../audio/bab2/bait40.mp3"
   },
   {
     "no": 41,
-    "text": "Seseorang memiliki seekor keledai tetapi tidak punya pelana: (begitu) ia mendapatkan pelana, serigala membawa pergi keledainya."
+    "text": "Seseorang memiliki seekor keledai tetapi tidak punya pelana: (begitu) ia mendapatkan pelana, serigala membawa pergi keledainya.",
+     "audio":"../audio/bab2/bait41.mp3"
   },
   {
     "no": 42,
-    "text": "Ia memiliki kendi, tetapi tak ada air yang dapat diperoleh: ketika ia menemukan air, kendi itu pecah."
+    "text": "Ia memiliki kendi, tetapi tak ada air yang dapat diperoleh: ketika ia menemukan air, kendi itu pecah.",
+     "audio":"../audio/bab2/bait42.mp3"
   },
   {
     "no": 43,
-    "text": "Raja menghimpun para tabib dari kiri dan kanan, berkata: \"Hidup kami berdua di tangan kalian."
+    "text": "Raja menghimpun para tabib dari kiri dan kanan, berkata: \"Hidup kami berdua di tangan kalian.",
+     "audio":"../audio/bab2/bait43.mp3"
   },
   {
     "no": 44,
-    "text": "Hidupku tiada arti; dialah nyawa bagi jiwaku. Aku terluka dan kesakitan: dialah obatku."
+    "text": "Hidupku tiada arti; dialah nyawa bagi jiwaku. Aku terluka dan kesakitan: dialah obatku.",
+     "audio":"../audio/bab2/bait44.mp3"
   },
   {
     "no": 45,
-    "text": "Siapa pun menyembuhkan Dia sang pemilik hidupku, niscaya membawa pulang harta dan mutiaraku, besar maupun kecil.\"⁴⁵"
+    "text": "Siapa pun menyembuhkan Dia sang pemilik hidupku, niscaya membawa pulang harta dan mutiaraku, besar maupun kecil.\"⁴⁵",
+     "audio":"../audio/bab2/bait45.mp3"
   },
   {
     "no": 46,
-    "text": "Mereka semua menjawab: \"Kami pertaruhkan nyawa, kami kumpulkan seluruh kecerdasan, kami satukan semua kemampuan."
+    "text": "Mereka semua menjawab: \"Kami pertaruhkan nyawa, kami kumpulkan seluruh kecerdasan, kami satukan semua kemampuan.",
+     "audio":"../audio/bab2/bait46.mp3"
   },
   {
     "no": 47,
-    "text": "Masing-masing kami adalah Al-Masih bagi dunia: di tangan kami tersedia obat bagi setiap duka.\""
+    "text": "Masing-masing kami adalah Al-Masih bagi dunia: di tangan kami tersedia obat bagi setiap duka.\"",
+     "audio":"../audio/bab2/bait47.mp3"
   },
   {
     "no": 48,
-    "text": "Dalam keangkuhan, mereka tak berucap \"<i>Jika Allah menghendaki\"</i>; maka Allah tunjukkan kepada mereka kelemahan manusia."
+    "text": "Dalam keangkuhan, mereka tak berucap \"<i>Jika Allah menghendaki\"</i>; maka Allah tunjukkan kepada mereka kelemahan manusia.",
+     "audio":"../audio/bab2/bait48.mp3"
   },
   {
     "no": 49,
-    "text": "Maksudku, melalaikan kalimat pengecualian itu tanda kerasnya hati; bukan sekadar lisan tak berucap, karena itu hanyalah kulit luar."
+    "text": "Maksudku, melalaikan kalimat pengecualian itu tanda kerasnya hati; bukan sekadar lisan tak berucap, karena itu hanyalah kulit luar.",
+     "audio":"../audio/bab2/bait49.mp3"
   },
   {
     "no": 50,
-    "text": "Betapa banyak orang yang tidak mengucapkan ungkapan penyelamat itu, namun jiwanya selaras dengan ruh dari ungkapan itu!⁵⁰"
+    "text": "Betapa banyak orang yang tidak mengucapkan ungkapan penyelamat itu, namun jiwanya selaras dengan ruh dari ungkapan itu!⁵⁰",
+     "audio":"../audio/bab2/bait50.mp3"
   },
   {
     "no": 51,
-    "text": "Semakin banyak obat dan penawar yang mereka terapkan, semakin bertambah penyakit itu, dan kebutuhan itu tidak terpenuhi."
+    "text": "Semakin banyak obat dan penawar yang mereka terapkan, semakin bertambah penyakit itu, dan kebutuhan itu tidak terpenuhi.",
+     "audio":"../audio/bab2/bait51.mp3"
   },
   {
     "no": 52,
-    "text": "Gadis yang sakit itu menjadi (kurus) seperti sehelai rambut, (sementara) mata sang raja mengalirkan air mata darah seperti sungai."
+    "text": "Gadis yang sakit itu menjadi (kurus) seperti sehelai rambut, (sementara) mata sang raja mengalirkan air mata darah seperti sungai.",
+     "audio":"../audio/bab2/bait52.mp3"
   },
   {
     "no": 53,
-    "text": "Oleh takdir Ilahi, oksimel justru menghasilkan empedu, dan minyak almond malah menambah kekeringan."
+    "text": "Oleh takdir Ilahi, oksimel justru menghasilkan empedu, dan minyak almond malah menambah kekeringan.",
+     "audio":"../audio/bab2/bait53.mp3"
   },
   {
     "no": 54,
-    "text": "Dari (pemberian) myrobalan (ramuan jamu) timbul sembelit, kelonggaran pun berhenti; dan air justru memberi makan api, seperti nafta."
+    "text": "Dari (pemberian) myrobalan (ramuan jamu) timbul sembelit, kelonggaran pun berhenti; dan air justru memberi makan api, seperti nafta.",
+     "audio":"../audio/bab2/bait54.mp3"
   }
 ]
   
